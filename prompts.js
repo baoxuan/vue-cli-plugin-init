bd8b0f9d3cffaf86ab01abf0faa52d406a39e97d
baoxuan <baoxuan@qbb6.com>
2019-03-26 15:20:22 +0800

    新增配置

--- prompts.js
+++ prompts.js
@@ -1,18 +1,7 @@
 module.exports = [{
-  name: 'qbbUI',
-  type: 'confirm',
-  message: `import qbbUI?`,
-  default: false
-}, {
   name: 'layoutType',
   type: 'list',
   message: 'which layout you want?',
   choices: ['vw', 'rem', 'none'],
   default: 0
-}, {
-  name: 'checkbox',
-  type: 'checkbox',
-  message: 'what size you want to do?',
-  choices: ['x', 'xx', 'xxx'],
-  default: ['x']
 }]
